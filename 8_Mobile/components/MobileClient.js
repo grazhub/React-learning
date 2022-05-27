cc78ad4520f92d019db5b574668f905a2c25d939
glebeckaya <glebeckaya@xbsoftware.by>
2022-05-27 11:33:59 +0300

    [dev] Task 8

---
+++ 8_Mobile/components/MobileClient.js
@@ -0,0 +1,83 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { clientEvents } from './events';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+	static propTypes = {
+		info: PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			fam: PropTypes.string.isRequired,
+			im: PropTypes.string.isRequired,
+			otch: PropTypes.string.isRequired,
+			balance: PropTypes.number.isRequired,
+		}),
+	};
+
+	constructor(props) {
+		super(props);
+
+		this.newFamRef = React.createRef();
+		this.newImRef = React.createRef();
+		this.newOtchRef = React.createRef();
+		this.newBalanceRef = React.createRef();
+	}  
+
+	state = {
+		fam: this.props.info.fam,
+		im: this.props.info.im,
+		otch: this.props.info.otch,
+		balance: this.props.info.balance,
+		mode: this.props.info.mode || "readonly",
+		status: this.props.info.balance >= 0 ? "active" : "blocked",
+	};
+
+	editClient = () => {
+		if(this.state.mode === "edit") {
+			const newBalance = this.newBalanceRef.current && Number(this.newBalanceRef.current.value) || Number(this.state.balance);
+			const newProps = {
+				mode: "readonly",
+				fam: this.newFamRef.current && this.newFamRef.current.value || this.state.fam,
+				im: this.newImRef.current && this.newImRef.current.value || this.state.im,
+				otch: this.newOtchRef.current && this.newOtchRef.current.value || this.state.otch,
+				balance: newBalance,
+				status: newBalance < 0 ? "blocked" : "active",
+			}
+			this.setState(newProps);
+			clientEvents.emit("afterEditEnd", { id: this.props.info.id, fam: newProps.fam, im: newProps.im, otch: newProps.otch, balance: newProps.balance });
+		} else {
+			this.setState({ mode: "edit" });
+			clientEvents.emit("afterEditStart", this.props.info.id);
+		}
+	};
+	cancelEditor = () => this.setState({ mode: "readonly" });
+
+	removeClient = () => {
+		clientEvents.emit("removeClient", this.props.info.id);
+	}
+
+	render() {
+		console.log("MobileClient id="+this.props.info.id+" render");
+		
+		return (
+		<tr className='MobileClient'>
+				<td>{ this.state.mode === "edit" ? <input defaultValue = { this.state.fam } ref = { this.newFamRef }/> : this.state.fam }</td>
+				<td>{ this.state.mode === "edit" ? <input defaultValue = { this.state.im } ref = { this.newImRef }/> : this.state.im }</td>
+				<td>{ this.state.mode === "edit" ? <input defaultValue = { this.state.otch } ref = { this.newOtchRef }/> : this.state.otch }</td>
+				<td>{ this.state.mode === "edit" ? <input defaultValue = { this.state.balance } ref = { this.newBalanceRef }/> : this.state.balance }</td>
+				<td>{ this.state.status }</td>
+				<td>
+					<button onClick = { this.editClient }>{ this.state.mode === "edit" ? "Сохранить" : "Редактировать" }</button>
+					{ this.state.mode === "edit" ? <button onClick = { this.cancelEditor }>Отменить</button> : null }
+				</td>
+				<td>
+					<button onClick = { this.removeClient }>Удалить</button>
+				</td>
+		</tr>
+		);
+	}
+
+}
+
+export default MobileClient;
---
+++ 8_Mobile/components/MobileCompany.js
@@ -0,0 +1,136 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { clientEvents } from './events';
+
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+class MobileCompany extends React.PureComponent {
+	static propTypes = {
+		name: PropTypes.string.isRequired,
+		clients: PropTypes.arrayOf(
+			PropTypes.shape({
+				id: PropTypes.number.isRequired,
+				fam: PropTypes.string.isRequired,
+				im: PropTypes.string.isRequired,
+				otch: PropTypes.string.isRequired,
+				balance: PropTypes.number.isRequired,
+			})
+		),
+	};
+
+	state = {
+		name: this.props.name,
+		clients: this.props.clients,
+		filter: "",
+	};
+
+	componentDidMount = () => {
+		clientEvents.addListener('removeClient', this.removeClient);
+		clientEvents.addListener('afterEditEnd', this.endEditing);
+	};
+	
+	componentWillUnmount = () => {
+		clientEvents.removeListener('removeClient', this.removeClient);
+		clientEvents.removeListener('afterEditEnd', this.endEditing);
+	};
+
+	removeClient = id => {
+		const newClients = structuredClone(this.state.clients);
+		const clientInd = newClients.findIndex(item => item.id === id);
+		newClients.splice(clientInd, 1);
+		this.setState({ clients: newClients });
+	};
+
+	endEditing = client => {
+		const newClients = this.state.clients.slice();
+		let ind;
+		const oldClient = newClients.find((item, index) => {
+			if (item.id === client.id) {
+				ind = index;
+				return true;
+			};
+		});
+		const newClient = {
+			...oldClient,
+			...client
+		}
+		newClients.splice(ind, 1, newClient);
+		this.setState({ clients: newClients });
+	};
+
+	addEmptyClient = () => {
+		this.setState({ clients: this.state.clients.concat({ id: +(new Date()), fam: "", im: "", otch: "", balance: 0, mode: "edit" }) });
+	};
+
+	setName1 = () => {
+		this.setState({ name:'МТС' });
+	};
+
+	setName2 = () => {
+		this.setState({name:'Velcom'});
+	};
+
+	resetFilter = () => {
+		this.setState({ filter: "" });
+	};
+	setActiveFilter = () => {
+		this.setState({ filter: "active" });
+	};
+	setBlockedFilter = () => {
+		this.setState({ filter: "blocked" });
+	};
+
+	getClients = () => {
+		const clients = this.state.clients.slice();
+		let renderClients = clients;
+		if (this.state.filter === "active") {
+			renderClients = clients.filter(item => item.balance >= 0);
+		} else if (this.state.filter === "blocked") {
+			renderClients = clients.filter(item => item.balance < 0);
+		}
+		return renderClients.map( client => {
+			return <MobileClient key = { client.id } info = { client } />;
+		});
+	};
+
+	render() {
+		console.log("MobileCompany render");
+
+		return (
+		<div className='MobileCompany'>
+			<div className = "MobileCompanyNameBlok">
+				<input type="button" value="=МТС" onClick={ this.setName1 } />
+				<input type="button" value="=Velcom" onClick={ this.setName2 } />
+				<div className='MobileCompanyName'>Компания &laquo;{this.state.name}&raquo;</div>
+			</div>
+
+			<div className = "MobileCompanyNameBlok">
+				<input type="button" value="Все" onClick = { this.resetFilter } />
+				<input type="button" value="Активные" onClick = { this.setActiveFilter } />
+				<input type="button" value="Заблокированные" onClick = { this.setBlockedFilter } />
+			</div>
+
+			<table className = "MobileCompanyTable">
+				<tbody>
+					<tr className = "MobileCompanyTableHeader">
+						<th>Фамилия</th>
+						<th>Имя</th>
+						<th>Отчество</th>
+						<th>Баланс</th>
+						<th>Статус</th>
+						<th>Редактировать</th>
+						<th>Удалить</th>
+					</tr>
+					{ this.getClients() }
+				</tbody>
+			</table>
+			<input type="button" value="Добавить клиента" onClick={ this.addEmptyClient } />
+		</div>
+		);
+	}
+
+}
+
+export default MobileCompany;
